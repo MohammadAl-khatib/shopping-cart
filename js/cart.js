f34373dcebe7e1bedf4c3e204ba780615d1c1d5e
Emadeddin-Beshtawi <engemadeddin@gmail.com>
2021-08-11 14:24:42 +0300

    up333333

--- js/cart.js
+++ js/cart.js
@@ -10,13 +10,6 @@ function loadCart() {
   const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
   cart = new Cart(cartItems);
 
-//   for (let i = 0; i < cartItems.length; i++) {
-//     cart.push(new CartItem)
-
-//     cartItems.items[i].product
-//     cartItems.items[i].quantity
-
-// }
 
 }
 
@@ -28,7 +21,10 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  let tbodyEl = document.getElementById('tbody');
+ tbodyEl.textContent = '';
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
@@ -39,6 +35,29 @@ function showCart() {
   // TODO: Create a TR
   // TODO: Create a TD for the delete link, quantity,  and the item
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
+
+  let tbodyEl = document.getElementById('tbody');
+  table.appendChild(tbodyEl);
+
+  for (let i = 0; i < cart.items.length; i++) {
+    let trEl = document.createElement('tr');
+    tbodyEl.appendChild(trEl);
+
+    td1El = document.createElement('td');
+    let td2El = document.createElement('td');
+    let td3El = document.createElement('td');
+
+    trEl.appendChild(td1El);
+    trEl.appendChild(td2El);
+    trEl.appendChild(td3El);
+
+
+    td1El.textContent = 'X';
+    td1El.id = i;
+    td2El.textContent = cart.items[i].quantity;
+    td3El.textContent = cart.items[i].product;
+
+  }
 
 }
 
